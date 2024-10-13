8d7b08ebfd3a7384e2d91510320c5f843dacf7af
JafarThwahrah <jaffardawahreh2@gmail.com>
2024-10-14 00:30:56 +0300

    edit main

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(3000);
